84e4573a80813e23df19fb81406b89c4906c6627
iroshan <iroshanhm@gmail.com>
2022-11-04 11:10:49 +0000

    Add new line for checking the push

--- Learning-Git/src/Main.java
+++ Learning-Git/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Iroshan!");
+        System.out.println("This is git push validation");
     }
 }
