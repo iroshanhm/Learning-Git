a26c987c5959017379398d03e293deb3c158534f
Iroshan Herath <iroshanhm@gmail.com>
2022-11-04 11:22:45 +0000

    Update Main.java

--- Learning-Git/src/Main.java
+++ Learning-Git/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Iroshan!");
         System.out.println("This is git push validation");
+        System.out.println("Hello Herath");
     }
 }
